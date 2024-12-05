08fab16b3824a7eacc1b15ffa522d3e10155f8d1
James McCollum <jmccollum@signet-studios.com>
2024-12-05 06:51:24 -0600

    Initial commit

---
+++ Day 04/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+var input = File.ReadAllLines("./input.txt");
+
+void Part1(){
+    int[] N = new[] { 0, 1, 2, 3 };
+
+    var arr = input.Select(x => x.ToCharArray()).ToList();
+
+    var yMax = arr.Count;
+    var xMax = arr[0].Count();
+
+    int count = 0;
+    for (int y = 0; y < yMax; y++)
+    {
+        for (int x = 0; x < xMax; x++)
+        {
+            List<char[]> s = new();
+            if (x <= xMax - 4)
+                s.Add(N.Select(i => arr[y][x + i]).ToArray());
+            if (y <= yMax - 4)
+                s.Add(N.Select(i => arr[y + i][x]).ToArray());
+            if (x <= xMax - 4 && y <= yMax - 4)
+                s.Add(N.Select(i => arr[y + i][x + i]).ToArray());
+            if (x >= 3 && y <= yMax - 4)
+                s.Add(N.Select(i => arr[y + i][x - i]).ToArray());
+            count += s.AsEnumerable().Count(t => t.SequenceEqual("XMAS") || t.SequenceEqual("SAMX"));
+        }
+    }
+
+    //2398
+    Console.WriteLine($"Part 1: {count}");
+}
+
+void Part2(){
+    int[] N = new[] { 0, 1, 2, 3 };
+    string[] grid = input.ToArray();
+    int[] dx = new[] { 1, 1, -1, -1 };
+    int[] dy = new[] { 1, -1, 1, -1 };
+    int count = 0;
+    for (int y = 1; y <= grid.Length - 2; y++)
+    {
+        for (int x = 1; x <= grid[0].Length - 2; x++)
+        {
+            if (grid[y][x] != 'A') 
+                continue;
+            char[] nxts = N.Select(i => grid[y + dy[i]][x + dx[i]]).ToArray();
+            if (nxts.All(n => n == 'M' || n == 'S') && nxts[0] != nxts[3] && nxts[1] != nxts[2])
+                count += 1;
+        }
+    }
+
+    Console.WriteLine($"Part 2: {count}");
+}
+
+Part1();
+Part2();
---
+++ Day 05/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.ComponentModel.DataAnnotations;
+
+var input = File.ReadAllLines("./input.txt");
+// var input = File.ReadAllLines("./sample.txt");
+
+int Part1(){
+    var rules = input.Where(x => x.Contains('|')).Select(x => x.Split('|').ToList()).ToList();
+    var pages = input.Where(x => x.Contains(',')).Select(x => x.Split(',').ToList()).ToList();
+
+    var middleSum = 0;
+
+    foreach (var pageList in pages){
+        var valid = true;
+        Console.WriteLine($"Checking {string.Join(',', pageList)}...");
+
+        var applicableRules = rules.Where(x => pageList.Contains(x[0]) && pageList.Contains(x[1])).ToList();
+
+        foreach (var rule in applicableRules){
+            if (pageList.IndexOf(rule[1]) <= pageList.IndexOf(rule[0])){
+                Console.WriteLine($"--{rule[1]} is before {rule[0]} - {string.Join(',', pageList)} is invalid page order.");
+                valid = false;
+                break;
+            }
+        }
+
+        if (!valid) continue;
+
+        var middleNum = int.Parse(pageList[(pageList.Count - 1) / 2]);
+        Console.WriteLine($"{string.Join(',', pageList)} valid. Adding {middleNum} to total");
+
+        middleSum += middleNum;
+    }
+
+    Console.WriteLine($"Part 1: {middleSum}");
+    return middleSum;
+}
+
+int Part2(){
+    var rules = input.Where(x => x.Contains('|')).Select(x => x.Split('|').ToList()).ToList();
+    var pages = input.Where(x => x.Contains(',')).Select(x => x.Split(',').ToList()).ToList();
+
+    var middleSum = 0;
+
+    (bool result, List<string>? failedRule) CheckRules(List<List<string>> rules, List<string> pageList){
+        foreach (var rule in rules){
+            if (pageList.IndexOf(rule[1]) <= pageList.IndexOf(rule[0])){
+                // Console.WriteLine($"--{rule[1]} is before {rule[0]} - {string.Join(',', pageList)} is invalid page order.");
+                return (false, rule);
+            }
+        }
+        return (true, null);
+    }
+
+    foreach (var pageList in pages){
+        Console.WriteLine($"Checking {string.Join(',', pageList)}...");
+
+        var applicableRules = rules.Where(x => pageList.Contains(x[0]) && pageList.Contains(x[1])).ToList();
+
+        var (valid, breakingRule) = CheckRules(applicableRules, pageList);
+        if (valid) continue;
+
+        var currentPageSet = pageList.ToList();
+
+        while (!valid){
+            var idx0 = currentPageSet.IndexOf(breakingRule![0]);
+            var idx1 = currentPageSet.IndexOf(breakingRule[1]);
+
+            currentPageSet[idx0] = breakingRule[1];
+            currentPageSet[idx1] = breakingRule[0];
+            
+            (valid, breakingRule) = CheckRules(applicableRules, currentPageSet);
+        }
+
+        var middleNum = int.Parse(currentPageSet[(currentPageSet.Count - 1) / 2]);
+        Console.WriteLine($"{string.Join(',', currentPageSet)} is the valid order. Adding {middleNum} to total");
+
+        middleSum += middleNum;
+    }
+
+    // Console.WriteLine($"Part 2: {middleSum}");
+    return middleSum;
+}
+
+//var p1 = Part1(); Console.WriteLine($"Part 1: {p1}");
+
+var p2 = Part2(); Console.WriteLine($"Part 2: {p2}");
