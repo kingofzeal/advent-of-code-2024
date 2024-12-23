0ffee0292f541e6ea6351fc36e32e35ad86caec1
James McCollum <jmccollum@signet-studios.com>
2024-12-22 19:01:17 -0600

    Day 19/22

---
+++ Day 19/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+
+var inputFile = File.ReadAllLines("./input.txt");
+//var inputFile = File.ReadAllLines("./sample.txt");
+
+var input = inputFile.ToList();
+
+(long pt1, long pt2) Part1()
+{
+	var towels = input[0].Split(',').Select(x => x.Trim()).ToList();
+	var combos = input[2..];
+
+	var known = new Dictionary<string, long> { { "", 1 } };
+
+	long Check(string val)
+	{
+		if (known.ContainsKey(val))
+		{
+			return known[val];
+		}
+
+		var matchTowels = towels.Where(val.StartsWith).ToList();
+
+		if (!matchTowels.Any())
+		{
+			known.Add(val, 0);
+			return 0;
+		}
+
+		var sum = 0L;
+
+		foreach (var towel in matchTowels)
+		{
+			var sub = Check(val.Substring(towel.Length));
+
+			sum += sub;
+		}
+
+		known.Add(val, sum);
+		return sum;
+	}
+
+	var count = 0;
+	var allCombos = 0L;
+
+	foreach (var combo in combos)
+	{
+		Console.Write(combo);
+		var patterns = Check(combo);
+		if (patterns > 0)
+		{
+			count++;
+			allCombos += patterns;
+		}
+		Console.WriteLine($" {patterns}");
+	}
+
+	return (count, allCombos);
+}
+
+long Part2()
+{
+	return 0;
+}
+
+long p1 = 0;
+long p2 = 0;
+
+(p1, p2) = Part1();
+//p2 = Part2();
+
+Console.WriteLine();
+Console.WriteLine($"Part 1: {p1}");
+Console.WriteLine($"Part 2: {p2}");
---
+++ Day 22/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+
+var inputFile = File.ReadAllLines("./input.txt");
+//var inputFile = File.ReadAllLines("./sample.txt");
+
+var input = inputFile.Select(long.Parse).ToList();
+
+long Mix(long num1, long num2)
+{
+	return num1 ^ num2;
+}
+
+long Prune(long value, long pruneVal = 16777216)
+{
+	return value % pruneVal;
+}
+
+long GenerateSecret(long val)
+{
+	var newVal = val * 64;
+	newVal = Mix(newVal, val);
+	val = Prune(newVal);
+
+	newVal = val / 32;
+	newVal = Mix(newVal, val);
+	val = Prune(newVal);
+
+	newVal = val * 2048;
+	newVal = Mix(newVal, val);
+
+	return Prune(newVal);
+}
+
+long Part1()
+{
+	var total = 0L;
+
+	foreach (var val in input)
+	{
+		var tmpVal = val;
+		for (var i = 0; i < 2000; i++)
+		{
+			tmpVal = GenerateSecret(tmpVal);
+		}
+
+		Console.WriteLine(tmpVal);
+		total += tmpVal;
+	}
+
+	return total;
+}
+
+long Part2()
+{
+	var buyersLists = new Dictionary<long, List<long>>();
+
+	foreach (var val in input)
+	{
+		buyersLists.Add(val, new List<long> { val });
+		var tmpVal = val;
+
+		for (var i = 0; i < 2000; i++)
+		{
+			tmpVal = GenerateSecret(tmpVal);
+			buyersLists[val].Add(tmpVal);
+		}
+	}
+
+	var resultSet = new Dictionary<long, List<((int v1, int v2, int v3, int v4), int price)>>();
+
+	for (var i = 0; i < buyersLists.Count; i++)
+	{
+		resultSet.Add(input[i], new List<((int v1, int v2, int v3, int v4), int price)>());
+
+		for (var j = 1; j < buyersLists[input[i]].Count - 3; j++)
+		{
+			var process = ((int)(buyersLists[input[i]][j] % 10 - buyersLists[input[i]][j - 1] % 10),
+				(int)(buyersLists[input[i]][j + 1] % 10 - buyersLists[input[i]][j] % 10),
+				(int)(buyersLists[input[i]][j + 2] % 10 - buyersLists[input[i]][j + 1] % 10),
+				(int)(buyersLists[input[i]][j + 3] % 10 - buyersLists[input[i]][j + 2] % 10));
+
+			resultSet[input[i]].Add((process, (int)(buyersLists[input[i]][j + 3] % 10)));
+		}
+	}
+
+	var maxPrice = 0;
+	var flattened = resultSet.SelectMany(x => x.Value).ToList();
+
+	var byGroup = flattened.Where(x => x.Item1 != default).GroupBy(x => x.Item1).OrderByDescending(x => x.Count());
+
+	foreach (var item in byGroup)
+	{
+		var maxPossible = flattened.Count(x => x.Item1 == item.Key);
+		if (maxPossible * 9 < maxPrice)
+		{
+			Console.WriteLine($"{item.Key}: Not possible: only {maxPossible}, current max {maxPrice}");
+			//No way possible any more, all other entries will have less than this
+			break;
+		}
+
+		var totalPrice = 0;
+		var ct = 0;
+
+		for (var i = 0; i < resultSet.Count; i++)
+		{
+			var firstMatch = resultSet[input[i]].FirstOrDefault(x => x.Item1 == item.Key);
+
+			if (firstMatch != default)
+			{
+				totalPrice += firstMatch.price;
+				ct++;
+			}
+		}
+
+		Console.WriteLine($"{item.Key.ToString().PadLeft(16)} has {ct:000} of {maxPossible:000} max earning {totalPrice:0000} (current leader {maxPrice} - {maxPrice / 9} limit).");
+
+		if (totalPrice > maxPrice)
+		{
+			maxPrice = totalPrice;
+		}
+	}
+
+	return maxPrice;
+}
+
+long p1 = 0;
+long p2 = 0;
+
+p1 = Part1();
+p2 = Part2();
+
+Console.WriteLine();
+Console.WriteLine($"Part 1: {p1}");
+Console.WriteLine($"Part 2: {p2}");
